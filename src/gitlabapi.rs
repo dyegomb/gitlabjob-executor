d67e02daaa17f72e4a296046091d18ffe83b3bf2
dyegomb <dyegomb@gmail.com>
2023-03-28 15:37:28 -0300

    async with tokio

--- src/gitlabapi.rs
+++ src/gitlabapi.rs
@@ -1,53 +1,59 @@
 //https://docs.gitlab.com/ee/api/rest/index.html
 use crate::load_config::Config;
-use log::{debug, info, warn, error};
-use reqwest::header::{HeaderValue, HeaderMap};
+use log::{debug, error, info, warn};
+use reqwest::header::{HeaderMap, HeaderValue};
 
 // extern crate reqwest;
 
-
 pub enum JobScope {
-    Created, Pending, Running, Failed, 
-    Success, Canceled, Skipped, WaitingForResource, 
+    Created,
+    Pending,
+    Running,
+    Failed,
+    Success,
+    Canceled,
+    Skipped,
+    WaitingForResource,
     Manual,
 }
 
 fn api_builder(config: &Config) -> reqwest::ClientBuilder {
-
     let mut headers = HeaderMap::new();
-    headers.insert("PRIVATE-TOKEN", HeaderValue::from_str(config.private_token.as_ref().unwrap()).unwrap());
+    headers.insert(
+        "PRIVATE-TOKEN",
+        HeaderValue::from_str(config.private_token.as_ref().unwrap()).unwrap(),
+    );
     headers.insert("Accept", HeaderValue::from_str("application/json").unwrap());
 
-    reqwest::ClientBuilder::new()
-        .default_headers(headers)
-
+    reqwest::ClientBuilder::new().default_headers(headers)
 }
 
 fn api_get(config: &Config, url: &'static str) -> reqwest::RequestBuilder {
-
     let uri = config.base_url.as_ref().unwrap().clone() + url;
 
     match api_builder(config).build() {
         Ok(getter) => getter.get(uri),
         Err(err) => {
             panic!("Coudn't construct the api getter. {}", err);
-        },
+        }
     }
 }
 
-pub fn get_grp_jobs(config: &Config, scope:JobScope) -> Vec<String> {
-    let uri = format!("/api/v4/projects/{}/jobs?pagination=keyset&per_page=100&order_by=id&sort=asc", config.group_id.unwrap());
+pub fn get_grp_jobs(config: &Config, scope: JobScope) -> Vec<String> {
+    let uri = format!(
+        "/api/v4/projects/{}/jobs?pagination=keyset&per_page=100&order_by=id&sort=asc",
+        config.group_id.unwrap()
+    );
 
     unimplemented!()
 }
-
 
 #[cfg(test)]
 mod test_http {
     use crate::load_config;
 
     use super::*;
-    use futures::executor::block_on;
+    use futures::{executor::block_on, TryFutureExt};
 
     fn init() {
         let _ = env_logger::builder()
@@ -59,16 +65,17 @@ mod test_http {
             .try_init();
     }
 
-    #[test]
-    fn test_api_get() {
+    // #[test]
+    #[tokio::test]
+    async fn test_api_get() {
         init();
         let config = load_config().unwrap();
 
-        let req = api_get(&config, "api/v4/projects").send();
+        // let req = api_get(&config, "api/v4/projects").send();
 
-        let resp = block_on(req).unwrap();
-        let text = block_on(resp.text());
-        println!("{}", text.unwrap());
+        // let resp = block_on(req).unwrap();
+        // let text = block_on(resp.text());
+        // println!("{}", text.unwrap());
         // println!("{:?}", req);
         // futures::executor::block_on(req);
         // match req() {
@@ -76,7 +83,18 @@ mod test_http {
         //     _ => (),
         // }
 
+        let response = async {
+            api_get(&config, "api/v4/projects")
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+        };
+
+        // let text = block_on(response).unwrap();
+        // println!("{:?}", text);
+        println!("{}", response.await.unwrap());
 
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use log::{debug, info, warn};
+use log::{debug, info, warn, error};
 use crate::load_config::load_config;
 
 mod load_config;
 mod mail_sender;
 mod gitlabapi;
+
+
+// Just a generic Result type to ease error handling for us. Errors in multithreaded
+// async contexts needs some extra restrictions
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+async fn app() -> Result<()> {
+    // I treat this as the `main` function of the async part of our program. 
+    todo!()
+}
+
 
 fn main() {
     env_logger::init();
@@ -16,15 +27,35 @@ fn main() {
     info!("Isso é um info");
 
     println!("Hello, world!");
+
+    // let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    match rt.block_on(app()) {
+        Ok(_) => info!("Done"),
+        Err(e) => error!("An error ocurred: {}", e),
+    };
+
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    fn init() {
+        let _ = env_logger::builder()
+            // Include all events in tests
+            .filter_level(log::LevelFilter::max())
+            // Ensure events are captured by `cargo test`
+            .is_test(true)
+            // Ignore errors initializing the logger if tests race to configure it
+            .try_init();
+    }
+
     #[test]
     fn test_show_conf() {
-        // println!("Current config is {:?}", load_config().unwrap());
+        init();
+        println!("Current config is {:?}", load_config().unwrap());
 
         
 
