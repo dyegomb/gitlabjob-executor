0073cd42f5beb8a9cb270cec8c080b5725f1e43e
dyegomb <dyegomb@gmail.com>
2023-03-28 17:39:43 -0300

    wip: parse json

--- src/gitlabapi.rs
+++ src/gitlabapi.rs
@@ -3,8 +3,7 @@ use crate::load_config::Config;
 use log::{debug, error, info, warn};
 use reqwest::header::{HeaderMap, HeaderValue};
 
-// extern crate reqwest;
-
+/// Jobs scopes: https://docs.gitlab.com/ee/api/jobs.html#list-project-jobs
 pub enum JobScope {
     Created,
     Pending,
@@ -17,35 +16,83 @@ pub enum JobScope {
     Manual,
 }
 
-fn api_builder(config: &Config) -> reqwest::ClientBuilder {
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        "PRIVATE-TOKEN",
-        HeaderValue::from_str(config.private_token.as_ref().unwrap()).unwrap(),
-    );
-    headers.insert("Accept", HeaderValue::from_str("application/json").unwrap());
-
-    reqwest::ClientBuilder::new().default_headers(headers)
-}
-
-fn api_get(config: &Config, url: &'static str) -> reqwest::RequestBuilder {
-    let uri = config.base_url.as_ref().unwrap().clone() + url;
-
-    match api_builder(config).build() {
-        Ok(getter) => getter.get(uri),
-        Err(err) => {
-            panic!("Coudn't construct the api getter. {}", err);
+impl JobScope {
+    fn to_str(&self) -> &'static str {
+        match self {
+            JobScope::Created => "created",
+            JobScope::Pending => "pending",
+            JobScope::Running => "running",
+            JobScope::Failed => "failed",
+            JobScope::Success => "success",
+            JobScope::Canceled => "canceled",
+            JobScope::Skipped => "skipped",
+            JobScope::WaitingForResource => "waiting_for_resource",
+            JobScope::Manual => "manual",
         }
     }
 }
 
-pub fn get_grp_jobs(config: &Config, scope: JobScope) -> Vec<String> {
-    let uri = format!(
-        "/api/v4/projects/{}/jobs?pagination=keyset&per_page=100&order_by=id&sort=asc",
-        config.group_id.unwrap()
-    );
+pub struct GitlabJOB {
+    config: Config,
+}
 
-    unimplemented!()
+impl GitlabJOB {
+    pub fn new(config: &Config) -> Self {
+        GitlabJOB {
+            config: config.clone(),
+        }
+    }
+
+    fn api_builder(&self) -> reqwest::ClientBuilder {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "PRIVATE-TOKEN",
+            HeaderValue::from_str(self.config.private_token.as_ref().unwrap()).unwrap(),
+        );
+        headers.insert("Accept", HeaderValue::from_str("application/json").unwrap());
+
+        reqwest::ClientBuilder::new().default_headers(headers)
+    }
+
+    fn gen_url(&self, path: &str) -> reqwest::Url {
+        
+        let new_uri = self.config.base_url.clone().unwrap() + path;
+
+        match reqwest::Url::parse(&new_uri) {
+            Ok(url) => url,
+            Err(error) => {
+                error!("Error while parsing url: {}", new_uri);
+                panic!("Error while parsing url \"{}\": {}", new_uri, error)
+            }
+        }
+    }
+
+    fn api_get(&self, url: &String) -> reqwest::RequestBuilder {
+        let uri = self.gen_url(url);
+
+        debug!("Building request for {uri}");
+
+        match self.api_builder().build() {
+            Ok(getter) => getter.get(uri),
+            Err(err) => {
+                panic!("Coudn't construct the api caller: {}", err);
+            }
+        }
+    }
+
+    pub async fn get_grp_jobs(&self, scope: JobScope) -> Vec<String> {
+        let uri = format!(
+            "/api/v4/projects/{}/jobs?pagination=keyset&per_page=100&order_by=id&sort=asc&scope={}",
+            self.config.group_id.unwrap(),
+            scope.to_str()
+        );
+        let resp = self.api_get(&uri);
+
+        // use serde_json
+        // https://betterprogramming.pub/how-to-work-with-json-in-rust-35ddc964009e?gi=734881859642
+
+        unimplemented!()
+    }
 }
 
 #[cfg(test)]
@@ -53,15 +100,11 @@ mod test_http {
     use crate::load_config;
 
     use super::*;
-    use futures::{executor::block_on, TryFutureExt};
 
     fn init() {
         let _ = env_logger::builder()
-            // Include all events in tests
             .filter_level(log::LevelFilter::max())
-            // Ensure events are captured by `cargo test`
             .is_test(true)
-            // Ignore errors initializing the logger if tests race to configure it
             .try_init();
     }
 
@@ -71,30 +114,30 @@ mod test_http {
         init();
         let config = load_config().unwrap();
 
-        // let req = api_get(&config, "api/v4/projects").send();
+        let api = GitlabJOB::new(&config);
 
-        // let resp = block_on(req).unwrap();
-        // let text = block_on(resp.text());
-        // println!("{}", text.unwrap());
-        // println!("{:?}", req);
-        // futures::executor::block_on(req);
-        // match req() {
-        //     resp => println!("{:?}", block_on(resp)),
-        //     _ => (),
-        // }
+        let response = api.api_get(&"api/v4/projects".to_string())
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await;
 
-        let response = async {
-            api_get(&config, "api/v4/projects")
-            .send()
-            .await
-            .unwrap()
-            .text()
-            .await
-        };
+        debug!("{}", response.unwrap());
+    }
 
-        // let text = block_on(response).unwrap();
-        // println!("{:?}", text);
-        println!("{}", response.await.unwrap());
+    #[tokio::test]
+    async fn test_get_group_projects() {
+        init();
+
+        let config = load_config().unwrap();
+
+        let api = GitlabJOB::new(&config);
+
+        let response = api.get_grp_jobs(JobScope::Success);
+
+        // debug!("Response: {}", response);
+
 
     }
 }
