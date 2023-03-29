4e58341c7e600fe5b15e247341d892bc08a77ee4
dyegomb <dyegomb@gmail.com>
2023-03-29 16:53:38 -0300

    get jobs list

--- src/gitlabapi.rs
+++ src/gitlabapi.rs
@@ -2,6 +2,7 @@
 use crate::load_config::Config;
 use log::{debug, error, info, warn};
 use reqwest::header::{HeaderMap, HeaderValue};
+use serde_json::Value;
 
 /// Jobs scopes: https://docs.gitlab.com/ee/api/jobs.html#list-project-jobs
 pub enum JobScope {
@@ -55,7 +56,6 @@ impl GitlabJOB {
     }
 
     fn gen_url(&self, path: &str) -> reqwest::Url {
-        
         let new_uri = self.config.base_url.clone().unwrap() + path;
 
         match reqwest::Url::parse(&new_uri) {
@@ -80,23 +80,62 @@ impl GitlabJOB {
         }
     }
 
-    pub async fn get_grp_jobs(&self, scope: JobScope) -> Vec<String> {
+    pub fn get_group_projs(&self) -> Vec<String> {
+        todo!()
+    }
+
+    pub async fn get_prj_jobs(
+        &self,
+        project: usize,
+        scope: JobScope,
+    ) -> Vec<Value> {
         let uri = format!(
-            "/api/v4/projects/{}/jobs?pagination=keyset&per_page=100&order_by=id&sort=asc&scope={}",
-            self.config.group_id.unwrap(),
+            "/api/v4/projects/{}/jobs?per_page=100&order_by=id&sort=asc&scope={}",
+            project.to_string(),
             scope.to_str()
         );
-        let resp = self.api_get(&uri);
 
-        // use serde_json
-        // https://betterprogramming.pub/how-to-work-with-json-in-rust-35ddc964009e?gi=734881859642
+        let resp = self.api_get(&uri).send().await.unwrap().text().await;
 
-        unimplemented!()
+        // let mut tmp_vec = vec![];
+        let parse_json;
+        if let Ok(got_resp) = resp {
+            parse_json = serde_json::from_str::<Value>(&got_resp);
+        } else {
+            panic!("Error parsing json response from {}", &uri);
+        };
+
+        let mut vec_jobs: Vec<Value> = vec![];
+
+        if let Ok(json) = parse_json {
+            match json.as_array() {
+                Some(vec_json) => {
+                    vec_json.iter().for_each(|proj| {
+                        let val = proj.clone();
+                        vec_jobs.push(val);
+                    });
+                }
+                None => {
+                    debug!("No jobs found for {}", uri);
+                    return vec![]
+                }
+            }
+        };
+
+        // debug!("First job gotten: {:?}", &vec_jobs[0]);
+
+        // vec_jobs.iter().for_each(|v| println!("{v}"));
+
+        vec_jobs
     }
 }
 
 #[cfg(test)]
 mod test_http {
+    use std::io::Write;
+
+    // use serde::Deserializer;
+
     use crate::load_config;
 
     use super::*;
@@ -116,14 +155,28 @@ mod test_http {
 
         let api = GitlabJOB::new(&config);
 
-        let response = api.api_get(&"api/v4/projects".to_string())
-                .send()
-                .await
-                .unwrap()
-                .text()
-                .await;
+        let response = api
+            .api_get(&"/api/v4/projects".to_string())
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await;
 
-        debug!("{}", response.unwrap());
+        // debug!("{}", response.as_ref().unwrap());
+
+        let parsed_json: Value = serde_json::from_str(response.as_ref().unwrap()).unwrap();
+
+        parsed_json.as_array().iter().for_each(|projects| {
+            projects.iter().for_each(|proj| {
+                debug!("Project ID: {}", proj["id"]);
+                debug!("Project links: {}", proj["_links"]);
+            });
+        });
+
+        let _ = std::fs::File::create("/tmp/test.json")
+            .unwrap()
+            .write(response.unwrap().as_bytes());
     }
 
     #[tokio::test]
@@ -134,10 +187,23 @@ mod test_http {
 
         let api = GitlabJOB::new(&config);
 
-        let response = api.get_grp_jobs(JobScope::Success);
+        // let response = api.get_grp_jobs(JobScope::Success);
 
         // debug!("Response: {}", response);
+    }
 
+    #[tokio::test]
+    async fn test_get_prj_jobs() {
+        init();
 
+        let config = load_config().unwrap();
+
+        let api = GitlabJOB::new(&config);
+
+        let response = api.get_prj_jobs(config.project_id.unwrap(), JobScope::Canceled);
+
+        response.await
+            .iter()
+            .for_each(|job| {debug!("Got job: {}", job["id"])});
     }
 }
