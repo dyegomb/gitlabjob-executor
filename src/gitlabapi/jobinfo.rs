46f141bc97c587a3481d99f4c3f80a81accb0d11
dyegomb <dyegomb@gmail.com>
2023-04-06 11:23:16 -0300

    impl jobscope from string

--- src/gitlabapi/jobinfo.rs
+++ src/gitlabapi/jobinfo.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+use std::convert::From;
+
 /// Jobs scopes: https://docs.gitlab.com/ee/api/jobs.html#list-project-jobs
 #[derive(Debug)]
 pub enum JobScope {
@@ -10,6 +13,39 @@ pub enum JobScope {
     Skipped,
     WaitingForResource,
     Manual,
+}
+
+impl Display for JobScope {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            JobScope::Created => write!(f, "created"),
+            JobScope::Pending => write!(f, "pending"),
+            JobScope::Running => write!(f, "running"),
+            JobScope::Failed => write!(f, "failed"),
+            JobScope::Success => write!(f, "success"),
+            JobScope::Canceled => write!(f, "canceled"),
+            JobScope::Skipped => write!(f, "skipped"),
+            JobScope::WaitingForResource => write!(f, "waiting_for_resource"),
+            JobScope::Manual => write!(f, "manual"),
+        }
+    }
+}
+
+impl From<String> for JobScope {
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "created" => JobScope::Created,
+            "pending" => JobScope::Pending,
+            "running" => JobScope::Running,
+            "failed" => JobScope::Failed,
+            "success" => JobScope::Success,
+            "canceled" => JobScope::Canceled,
+            "skipped" => JobScope::Skipped,
+            "waiting_for_resource" => JobScope::WaitingForResource,
+            "manual" => JobScope::Manual,
+            _ => JobScope::Skipped
+        }
+    }
 }
 
 #[derive(Debug)]
