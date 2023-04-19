741553696380d1028213efdb1540d986b09717da
dyegomb <dyegomb@gmail.com>
2023-04-19 17:10:02 -0300

    test to cancel pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use log::{debug, info, warn, error};
+// use log::{debug, error, info, warn};
 
 mod configloader;
-mod mailsender;
 mod gitlabapi;
+mod mailsender;
 
 use configloader::prelude::*;
+use gitlabapi::prelude::*;
 
 // Just a generic Result type to ease error handling for us. Errors in multithreaded
 // async contexts needs some extra restrictions
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 async fn app() -> Result<()> {
-
     let config = match Config::load_config() {
         Ok(conf) => conf,
         Err(err) => panic!("Error loading configurations. {}", err),
@@ -19,7 +19,6 @@ async fn app() -> Result<()> {
 
     Ok(())
 }
-
 
 fn main() {
     env_logger::init();
@@ -30,10 +29,9 @@ fn main() {
         .unwrap();
 
     match rt.block_on(app()) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => error!("An error ocurred: {}", e),
     };
-
 }
 
 #[cfg(test)]
@@ -55,5 +53,34 @@ mod test {
     fn test_show_conf() {
         // init();
         debug!("Current config is {:?}", Config::load_config().unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_multi_pipelines() {
+        init();
+
+        let config = Config::load_config().unwrap();
+
+        let api = GitlabJOB::new(config);
+
+        let multi_jobs = api.get_jobs_by_proj_and_pipeline(JobScope::Manual).await;
+
+        multi_jobs.iter()
+            .for_each(|(proj, pipes)| {
+                debug!("On project {}", proj);
+                let mut pipe_key: Vec<u64> = pipes.clone().into_keys().collect();
+
+                pipe_key.sort();
+                pipe_key.reverse();
+
+                pipe_key.iter()
+                    .skip(1)
+                    .for_each(|pipeid| {
+                        debug!("Cancel pipeline {} ?", pipeid);
+                    });
+                
+
+            })
+        
     }
 }
