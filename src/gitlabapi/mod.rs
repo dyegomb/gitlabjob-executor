4c4fb683cb9c7d378bf7ff5dce4732036cd37631
dyegomb <dyegomb@gmail.com>
2023-04-27 15:20:07 -0300

    fmt

--- src/gitlabapi/mod.rs
+++ src/gitlabapi/mod.rs
@@ -1,22 +1,21 @@
+mod gitlabjob;
 mod jobinfo;
 mod tests;
 mod utils;
-mod gitlabjob;
 
 /// Specify how many concurrent tasks
 pub const STREAM_BUFF_SIZE: usize = 10;
 
 pub mod prelude {
-    pub use serde_json::Value;
-    pub use reqwest::header::{HeaderMap, HeaderValue};
-    pub use log::{debug, error, warn};
-    pub use std::collections::{HashMap, HashSet};
-    pub use futures::join;
-    pub use futures::stream::{ self, StreamExt};
-    pub use crate::gitlabapi::gitlabjob::GitlabJOB;
-    pub use crate::gitlabapi::utils::{ApiUtils, HttpMethod};
-    pub use crate::gitlabapi::jobinfo::{JobInfo, JobScope};
-    pub use crate::configloader::Config;
     pub use super::STREAM_BUFF_SIZE;
+    pub use crate::configloader::Config;
+    pub use crate::gitlabapi::gitlabjob::GitlabJOB;
+    pub use crate::gitlabapi::jobinfo::{JobInfo, JobScope};
+    pub use crate::gitlabapi::utils::{ApiUtils, HttpMethod};
+    pub use futures::join;
+    pub use futures::stream::{self, StreamExt};
+    pub use log::{debug, error, info, warn};
+    pub use reqwest::header::{HeaderMap, HeaderValue};
+    pub use serde_json::Value;
+    pub use std::collections::{HashMap, HashSet};
 }
-
--- src/mailsender/mod.rs
+++ src/mailsender/mod.rs
@@ -1,21 +1,21 @@
+mod sender;
+mod smtpconfig;
 mod tests;
 mod utils;
-mod smtpconfig;
-mod sender;
 
 pub mod prelude {
     pub const DEFAULT_SMTP_PORT: u16 = 587;
 
-    pub use log::{debug, warn, error};
-    pub use merge::Merge;
-    pub use serde::Deserialize;
-    pub use lettre::{Message, SmtpTransport, Transport};
     pub use lettre::message::header::ContentType;
     pub use lettre::message::Mailbox;
     pub use lettre::transport::smtp::authentication::Credentials;
     pub use lettre::transport::smtp::client::{Tls, TlsParameters};
+    pub use lettre::{Message, SmtpTransport, Transport};
+    pub use log::{debug, error, warn};
+    pub use merge::Merge;
+    pub use serde::Deserialize;
 
-    pub use super::smtpconfig::SmtpConfig;
     pub use super::sender::MailSender;
+    pub use super::smtpconfig::SmtpConfig;
     pub use super::utils::SmtpUtils;
 }
