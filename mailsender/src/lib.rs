e389233a59f564aa3aea53698cc92b7985bd78f9
dyegomb <dyegomb@gmail.com>
2023-12-03 09:45:41 -0300

    mailsender ok

--- mailsender/src/lib.rs
+++ mailsender/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod sender;
+// mod smtpconfig;
+mod tests;
+mod utils;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod prelude {
+    pub const DEFAULT_SMTP_PORT: u16 = 587;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    pub use lettre::message::header::ContentType;
+    pub use lettre::message::{Mailboxes, MessageBuilder};
+    pub use lettre::transport::smtp::authentication::Credentials;
+    pub use lettre::transport::smtp::client::{Tls, TlsParameters};
+    pub use lettre::{Message, SmtpTransport, Transport};
+    pub use log::{debug, error, warn};
+    pub use merge::Merge;
+    pub use serde::Deserialize;
+
+    pub use super::sender::MailSender;
+    pub use configloader::SmtpConfig;
+    // pub use super::smtpconfig::SmtpConfig;
+    pub use super::utils::SmtpUtils;
 }
