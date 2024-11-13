b62e926e344aff91e1ee51df82a4061f9414abe3
Dyego <dyegomb@gmail.com>
2024-11-13 18:00:32 -0300

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rt = runtime::Builder::new_current_thread().build()?;
 
     rt.block_on(async {
-        // Set default log level for INFO, changed with "RUST_LOG" environment variable
+        // Set default log level to INFO, changed with "RUST_LOG" environment variable
         env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
         let config = match Config::load_config() {
