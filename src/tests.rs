8628507fb22e1e640302876dc60e659578f65227
Dyego <dyegomb@gmail.com>
2024-11-14 19:36:19 -0300

    use working tag

--- src/tests.rs
+++ src/tests.rs
@@ -45,8 +45,10 @@ mod integration_tests {
                     // "variables": {"trigger_email":"teste@test.tst"},
                     // "variables": {"ref_source":"master"},
                     // "variables": {"source_id":config.project_id.unwrap_or(0).to_string()},
-                    "variables": {"PROD_TAG": "Test-test-1.0.0",
-                        "trigger_email":"teste@test.tst"},
+                    "variables": {
+                        "PROD_TAG": "Test-tag",
+                        "trigger_email":"teste@test.tst"
+                    },
                 }
         );
 
